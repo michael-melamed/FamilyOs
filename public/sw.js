d5bcf57a584b67eb039bd7eed9c2f48d68002e49
michael-melamed <203123mty@gmail.com>
2026-07-08 14:23:59 +0300

    Register PWA service worker and add Apple/iOS web app meta tags

---
+++ public/sw.js
@@ -0,0 +1,12 @@
+self.addEventListener('install', function(event) {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', function(event) {
+  // Minimal fetch handler to satisfy PWA installation criteria
+  event.respondWith(fetch(event.request));
+});
